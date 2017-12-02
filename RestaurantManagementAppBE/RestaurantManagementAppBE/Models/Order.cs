35e4ac14f3bad33adba00ef43898e7e9d102dc5e
rahad123 <rahaddiu@gmail.com>
2017-12-02 14:00:22 +0600

    Added by Order and OrderItems

---
+++ RestaurantManagementAppBE/RestaurantManagementAppBE/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantManagementAppBE.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public List<OrderedItem> OrderItems { get; set; }
+    }
+}

501761c1b0011d305d6611cbd018bd80a6542eed
rahad123 <rahaddiu@gmail.com>
2017-12-02 15:47:13 +0600

    post method for food item

--- RestaurantManagementAppBE/RestaurantManagementAppBE/Controllers/RestaurantManagementAppController.cs
+++ RestaurantManagementAppBE/RestaurantManagementAppBE/Controllers/RestaurantManagementAppController.cs
@@ -18,17 +18,27 @@ namespace RestaurantManagementAppBE.Controllers
         {
             _Context = new MyDbContext();
         }
+
         [Route("api/menu")]
         [HttpGet]
         public MenuViewModel Menu()
         {
 
-            var foodItem = _Context.FoodItems.GroupBy(p=>p.Category).ToDictionary(p=>p.Key.Name, p=>p.ToList());
+            var foodItem = _Context.FoodItems.GroupBy(p => p.Category).ToDictionary(p => p.Key.Name, p => p.ToList());
             var setMenu = _Context.SetMenus.Include(a => a.SetMenuItems).ToList();
-            var menu = new MenuViewModel {  SetMenus = setMenu, FoodCategories = foodItem};
+            var menu = new MenuViewModel {SetMenus = setMenu, FoodCategories = foodItem};
             return menu;
         }
-        
-        
-    }
+
+        [HttpPost]
+        [Route("api/menu")]
+        public string Menu(FoodItem foodItem)
+        {
+            _Context.FoodItems.Add(foodItem);
+            _Context.SaveChanges();
+            return "Success";
+        }
+    
+
+}
 }

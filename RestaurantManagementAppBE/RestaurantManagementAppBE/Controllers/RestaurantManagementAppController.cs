af8356d1bfe57b82a37d95b598e422a6ce9e3d7e
rahad123 <rahaddiu@gmail.com>
2017-12-02 10:33:25 +0600

    Menu Viewmodel Return by Fooditem

---
+++ RestaurantManagementAppBE/RestaurantManagementAppBE/Controllers/RestaurantManagementAppController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestaurantManagementAppBE.Models;
+
+namespace RestaurantManagementAppBE.Controllers
+{
+    public class RestaurantManagementAppController : ApiController
+    {
+        public MyDbContext _Context;
+
+        public RestaurantManagementAppController()
+        {
+            _Context = new MyDbContext();
+        }
+        [Route("api/menu")]
+        [HttpGet]
+        public MenuViewModel Menu()
+        {
+
+            var foodItem = _Context.FoodItems.ToList();
+            var menu = new MenuViewModel {FoodItems = foodItem};
+            return menu;
+        }
+
+        
+    }
+}
---
+++ RestaurantManagementAppBE/RestaurantManagementAppBE/Models/MenuViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantManagementAppBE.Models
+{
+    public class MenuViewModel
+    {
+        public List<SetMenu> SetMenus { get; set; }
+        public List<FoodItem> FoodItems { get; set; }
+    }
+}

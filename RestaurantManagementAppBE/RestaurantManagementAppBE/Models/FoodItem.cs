e4e6cac1539afb04734d8607612896998cd41a93
SanjidHaque <sanjidulhaque@gmail.com>
2017-12-29 14:20:59 +0600

    New Backend is added (Please work on this project), Old backend is deleted

--- RestaurantManagementAppBE/RestaurantManagementAppBE/Models/FoodItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RestaurantManagementAppBE.Models
-{
-    public class FoodItem
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Price { get; set; }
-    }
-}

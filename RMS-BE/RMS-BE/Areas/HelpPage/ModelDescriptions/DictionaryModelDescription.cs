fcd8b702b2751e415cd520593e128c921e3c194c
SanjidHaque <sanjidulhaque@gmail.com>
2018-01-06 19:00:12 +0600

    deleted old project

--- RMS-BE/RMS-BE/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RMS_BE.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- RMS-BE/RMS-BE/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RMS_BE.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}

d1d8c27eb59c33952080d61a80c4d6f17227a856
SanjidHaque <sanjidulhaque@gmail.com>
2019-02-13 00:26:09 +0600

    "Button is auto disabled , gets enabled after a click" problem is solved in a bypassed way.

--- RestaurantManagementApp/src/app/login/login.component.ts
+++ RestaurantManagementApp/src/app/login/login.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {DataStorageService} from '../shared/data-storage.service';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {UserService} from '../user.service';
 import {Router} from '@angular/router';
 import {HttpErrorResponse} from '@angular/common/http';
@@ -11,22 +10,33 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('userLoginForm') form: NgForm;
+
   isLoginError = false;
+  isDisabled: boolean;
   constructor(private userService : UserService,
               private router : Router) { }
 
   ngOnInit() {
+    this.form.value.userName = '';
+    this.form.value.password = '';
 
+    if (this.form.value.userName === '' && this.form.value.password === '') {
+      this.isDisabled = true;
+    } else {
+      this.isDisabled = false;
+    }
   }
 
-  OnSubmit(form: NgForm) {
-     this.userService.userAuthentication(form.value.userName, form.value.password)
+  OnSubmit() {
+    this.isDisabled = false;
+     this.userService.userAuthentication(this.form.value.userName, this.form.value.password)
        .subscribe((data : any) => {
        console.log(data.json().access_token);
        console.log(data.json().role);
          localStorage.setItem('userToken', data.json().access_token);
          localStorage.setItem('userRoles', data.json().role);
-         localStorage.setItem('userName', form.value.userName);
+         localStorage.setItem('userName', this.form.value.userName);
          if (this.userService.roleMatch(['Cashier'])) {
            this.router.navigate(['our-offers/regulars']);
          } else {

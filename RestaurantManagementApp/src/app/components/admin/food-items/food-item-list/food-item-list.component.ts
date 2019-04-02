b95d0bb0c94a9f3152c21be580f8db0b0b4f1872
SanjidHaque <sanjidulhaque@gmail.com>
2019-04-02 23:39:03 +0600

    food and inventory's mat-table updated with border

--- RestaurantManagementApp/src/app/components/admin/food-items/food-item-list/food-item-list.component.ts
+++ RestaurantManagementApp/src/app/components/admin/food-items/food-item-list/food-item-list.component.ts
@@ -1,41 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+
 import {FoodItem} from '../../../../models/food-item.model';
-import {PointOfSaleService} from '../../../../services/point-of-sale.service';
 
 @Component({
   selector: 'app-food-item-list',
   templateUrl: './food-item-list.component.html',
   styleUrls: ['./food-item-list.component.scss']
 })
-export class FoodItemListComponent implements OnInit {
+export class FoodItemListComponent implements OnInit, AfterViewInit {
   foodItems: FoodItem[] = [];
-  total: number;
-  constructor(private route: ActivatedRoute,
-              private router: Router,
-              private pointOfSaleService: PointOfSaleService
-            ) { }
+
+  displayedColumns: string[] =
+    [
+      'SerialNumber',
+      'Name',
+      'Price',
+      'InventoryCost',
+      'Profit',
+      'TotalSale'
+    ];
+  dataSource: MatTableDataSource<FoodItem>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.
     subscribe(
       ( data: FoodItem[]) => {
-        this.pointOfSaleService.foodItems = data['foodItems'];
+        this.foodItems = data['foodItems'];
+        this.dataSource = new MatTableDataSource(this.foodItems);
+
       }
     );
-    this.foodItems = this.pointOfSaleService.foodItems;
-    this.pointOfSaleService.foodItemsChanged
-      .subscribe(
-        (FoodItem: FoodItem[]) => {
-          this.foodItems = FoodItem;
-        }
-      );
-    this.total = this.pointOfSaleService.foodItems.length;
   }
 
-  viewDetails(foodItem: FoodItem) {
-    const foodItemId =  foodItem.Id;
-    this.router.navigate(['admin/food-item/food-item-details', foodItemId]);
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
